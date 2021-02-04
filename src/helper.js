8566855b77390ae7771e4f17aa0faaa0c2c26ec1
Héctor Berumen <hberumen@uaz.edu.mx>
2021-02-03 21:44:55 -0600

    calcular costo basado en marca

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,23 @@
 export const obtenerDiferenciaYear = year => {
     return new Date().getFullYear() - year
 }
+
+export const calcularMarca = marca => {
+    let incremento
+
+    switch(marca){
+        case 'europeo':
+            incremento = 1.30
+            break
+        case 'americano':
+            incremento = 1.30
+            break
+        case 'asiatico':
+            incremento = 1.30
+            break
+        default: 
+            break
+    }
+
+    return incremento;
+}
